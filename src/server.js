28eca031e41d129f0cd950c9a6408987dd932d93
S. Patterson <bountyfiveo@gmail.com>
2022-12-20 05:14:08 -0500

    init push

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const { PORT = 5000 } = process.env;
 const app = require("./app");
-
+console.log(`goto http://localhost:${PORT} (press CTRL+C to quit)`);
 const listener = () => console.log(`Listening on Port ${PORT}!`);
+
 app.listen(PORT, listener);
